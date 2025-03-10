710f6f8ad4da1ce32961f7960d9f586042fbffef
michellejtan <michelle0223@gmail.com>
2025-03-10 10:52:26 -0700

    WIP: adds server boilderplate

--- server.js
+++ server.js
@@ -1,21 +1,30 @@
+// dependencies
 const dotenv = require("dotenv");
-dotenv.config();
+
 const express = require("express");
-const app = express();
 
 const mongoose = require("mongoose");
 const methodOverride = require("method-override");
 const morgan = require("morgan");
 
+
+// intialize express app
+const app = express();
+
+
+// configure settings
+dotenv.config();
+const port = process.env.PORT || "3000";  // catch when we deploy it
 // Set the port from environment variable or default to 3000
-const port = process.env.PORT ? process.env.PORT : "3000";
+// const port = process.env.PORT ? process.env.PORT : "3000"; //Dan feel this is word, ternity operator, 3 operamd
 
+// connect to mongoDB
 mongoose.connect(process.env.MONGODB_URI);
-
 mongoose.connection.on("connected", () => {
   console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
 });
 
+// mount middleware
 // Middleware to parse URL-encoded data from forms
 app.use(express.urlencoded({ extended: false }));
 // Middleware for using HTTP verbs such as PUT or DELETE
@@ -23,6 +32,8 @@ app.use(methodOverride("_method"));
 // Morgan for logging HTTP requests
 app.use(morgan('dev'));
 
+
+// tell the app to listen for HHTP requests
 app.listen(port, () => {
   console.log(`The express app is ready on port ${port}!`);
 });
